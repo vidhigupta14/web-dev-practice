156782867d0d3e77591d741231df79e468466d88
Vidhi <vg14vidhi@gmail.com>
2025-08-30 10:05:09 +0530

    express js practice 2

---
+++ practice express 2/script.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;  
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/search', (req,res)=>{
+    const term=req.query.term;
+    res.send(`Search Results for ${term}`)
+})
+
+app.get('/user', (req, res)=>{
+    res.json({
+        name: 'Vidhi',
+        age: 20,
+        role: 'learning-web-dev'
+    })
+})
+
+app.get('/user/:slug', (req, res)=>{
+    res.send(`Hello ${req.params.slug}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
